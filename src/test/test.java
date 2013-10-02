63f318b2bf8d3a8a6630ae5f0f5f81c0dfa7a76b
sunscode <sunscode@gmail.com>
2013-10-03 01:25:04 +0900

    5

--- src/test/test.java
+++ src/test/test.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	System.out.println("테스트 입니다.");
 	System.out.println("테스트 입니다.");
 	System.out.println("테스트 입니다.");
-
+	System.out.println("테스트 입니다.");
+	System.out.println("테스트 입니다.");
 }
 }
