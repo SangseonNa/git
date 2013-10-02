c5917371969b43bf47766ea9b20f459e3b24db9a
Sangseon, Na <sunscode@gmail.com>
2013-10-03 00:57:54 +0900

    test
    test

--- src/test/test.java
+++ src/test/test.java
@@ -4,7 +4,6 @@ public class test {
 public static void main(String[] args) {
 	System.out.println("테스트 입니다.");
 	System.out.println("테스트 입니다.");
-	System.out.println("테스트 입니다.");
-	System.out.println("테스트 입니다.");
+
 }
 }
