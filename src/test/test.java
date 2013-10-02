c60142ed69588ef2271511efb3a553178431ce21
sunscode <sunscode@gmail.com>
2013-10-03 00:56:06 +0900

    두번째 수정

--- src/test/test.java
+++ src/test/test.java
@@ -3,5 +3,8 @@ package test;
 public class test {
 public static void main(String[] args) {
 	System.out.println("테스트 입니다.");
+	System.out.println("테스트 입니다.");
+	System.out.println("테스트 입니다.");
+	System.out.println("테스트 입니다.");
 }
 }
